72f2e84e4a918c6180e27cfd7993b07a888bd033
Michał Walczak <magikmw@michalwalczak.eu>
2021-12-08 01:04:20 +0100

    adds detection of a wireless X360 controller

--- input_helper/input_helper.gd
+++ input_helper/input_helper.gd
@@ -38,7 +38,7 @@ func _input(event: InputEvent) -> void:
 
 func get_simplified_device_name(raw_name: String) -> String:
 	match raw_name:
-		"XInput Gamepad", "Xbox Series Controller":
+		"XInput Gamepad", "Xbox Series Controller", "Xbox 360 Wireless Receiver":
 			return DEVICE_XBOX_CONTROLLER
 		
 		"PS5 Controller", "PS4 Controller", "PS3 Controller", "PS2 Controller":
