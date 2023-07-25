e953344e04c252facec2b5a79bc0d2ab9559652a
Nathan Hoad <nathan@nathanhoad.net>
2023-07-25 22:27:28 +1000

    Add support for multiple keys/buttons per action (#15)

--- addons/input_helper/input_helper.gd
+++ addons/input_helper/input_helper.gd
@@ -123,6 +123,9 @@ func reset_all_actions() -> void:
 			action_mouse_button_changed.emit(action, mouse_button)
 
 
+### Keyboard input
+
+
 func is_valid_key(key: String) -> bool:
 	if key.length() == 1: return true
 	if key in [
@@ -141,17 +144,44 @@ func is_valid_key(key: String) -> bool:
 	return false
 
 
-func get_action_key(action: String) -> String:
+## Get all of the keys used for an action
+func get_action_keys(action: String) -> PackedStringArray:
+	var keys: PackedStringArray = []
 	for event in InputMap.action_get_events(action):
 		if event is InputEventKey:
 			if OS.get_keycode_string(event.physical_keycode):
-				return OS.get_keycode_string(event.physical_keycode)
+				keys.append(OS.get_keycode_string(event.physical_keycode))
 			else:
-				return event.as_text()
-	return ""
+				keys.append(event.as_text())
+	return keys
 
 
+## Get the first key for an action
+func get_action_key(action: String) -> String:
+	var keys: PackedStringArray = get_action_keys(action)
+	return "" if keys.is_empty() else keys[0]
+
+
+## Set the key used for an action
 func set_action_key(target_action: String, key: String, swap_if_taken: bool = true) -> Error:
+	return _update_action_key(target_action, key, swap_if_taken, "")
+
+
+## Replace a specific key with another key
+func replace_action_key(target_action: String, current_key: String, next_key: String, swap_if_taken: bool = true) -> Error:
+	return _update_action_key(target_action, next_key, swap_if_taken, current_key)
+
+
+## Replace a specific key, given its index
+func replace_action_key_at_index(target_action: String, index: int, next_key: String, swap_if_taken: bool = true) -> Error:
+	var keys: PackedStringArray = get_action_keys(target_action)
+
+	if keys.is_empty() or keys.size() < index: return ERR_INVALID_DATA
+
+	return _update_action_key(target_action, next_key, swap_if_taken, keys[index])
+
+
+func _update_action_key(target_action: String, key: String, swap_if_taken: bool, only_replace_key: String) -> Error:
 	if not is_valid_key(key): return ERR_INVALID_DATA
 
 	# Find any action that is already mapped to this key
@@ -159,6 +189,8 @@ func set_action_key(target_action: String, key: String, swap_if_taken: bool = tr
 	var clashing_event
 	if swap_if_taken:
 		for action in InputMap.get_actions():
+			if action == target_action: continue
+
 			for event in InputMap.action_get_events(action):
 				if event is InputEventKey and OS.get_keycode_string(event.physical_keycode) == key:
 					clashing_action = action
@@ -167,6 +199,9 @@ func set_action_key(target_action: String, key: String, swap_if_taken: bool = tr
 	# Find the key based event for the target action
 	for event in InputMap.action_get_events(target_action):
 		if event is InputEventKey:
+			if only_replace_key != "" and only_replace_key != OS.get_keycode_string(event.physical_keycode) and only_replace_key != event.as_text():
+				continue
+
 			# Add the current mapping to the clashing action
 			if clashing_action:
 				InputMap.action_erase_event(clashing_action, clashing_event)
@@ -183,15 +218,45 @@ func set_action_key(target_action: String, key: String, swap_if_taken: bool = tr
 	return OK
 
 
-func get_action_button(action: String) -> JoyButton:
-	# Get the first button input
+### Gamepad input
+
+
+## Get all buttons used for an action
+func get_action_buttons(action: String) -> Array[JoyButton]:
+	var buttons: Array[JoyButton] = []
 	for event in InputMap.action_get_events(action):
 		if event is InputEventJoypadButton:
-			return event.button_index
-	return JOY_BUTTON_INVALID
+			buttons.append(event.button_index)
+	return buttons
 
 
+## Get the first button for an action
+func get_action_button(action: String) -> JoyButton:
+	var buttons: Array[JoyButton] = get_action_buttons(action)
+	return JOY_BUTTON_INVALID if buttons.is_empty() else buttons[0]
+
+
+## Set the button for an action
 func set_action_button(target_action: String, button: JoyButton, swap_if_taken: bool = true) -> Error:
+	return _update_action_button(target_action, button, swap_if_taken)
+
+
+## Replace a specific button for an action
+func replace_action_button(target_action: String, current_button: JoyButton, next_button: JoyButton, swap_if_taken: bool = true) -> Error:
+	return _update_action_button(target_action, next_button, swap_if_taken, current_button)
+
+
+## Replace a button, given its index
+func replace_action_button_at_index(target_action: String, index: int, next_button: JoyButton, swap_if_taken: bool = true) -> Error:
+	var buttons: Array[JoyButton] = get_action_buttons(target_action)
+
+	if buttons.is_empty() or buttons.size() < index: return ERR_INVALID_DATA
+
+	return _update_action_button(target_action, next_button, swap_if_taken, buttons[index])
+
+
+## Set the action used for a button
+func _update_action_button(target_action: String, button: JoyButton, swap_if_taken: bool = true, only_replace_button: JoyButton = JOY_BUTTON_INVALID) -> Error:
 	# Find any action that is already mapped to this button
 	var clashing_action = ""
 	var clashing_event
@@ -205,6 +270,9 @@ func set_action_button(target_action: String, button: JoyButton, swap_if_taken: 
 	# Find the button based event for the target action
 	for event in InputMap.action_get_events(target_action):
 		if event is InputEventJoypadButton:
+			if only_replace_button != JOY_BUTTON_INVALID and only_replace_button != button:
+				continue
+
 			# Add the current mapping to the clashing action
 			if clashing_action:
 				InputMap.action_erase_event(clashing_action, clashing_event)
@@ -221,15 +289,45 @@ func set_action_button(target_action: String, button: JoyButton, swap_if_taken: 
 	return OK
 
 
-func get_action_mouse_button(action: String) -> MouseButton:
-	# Get the first mouse input
+### Mouse input
+
+
+## Get all mouse buttons for an action
+func get_action_mouse_buttons(action: String) -> Array[MouseButton]:
+	var buttons: Array[MouseButton] = []
 	for event in InputMap.action_get_events(action):
 		if event is InputEventMouseButton:
-			return event.button_index
-	return MOUSE_BUTTON_NONE
+			buttons.append(event.button_index)
+
+	return buttons
 
 
+## Get the first mouse buton for an action
+func get_action_mouse_button(action: String) -> MouseButton:
+	var buttons: Array[MouseButton] = get_action_mouse_buttons(action)
+	return MOUSE_BUTTON_NONE if buttons.is_empty() else buttons[0]
+
+
+## Set the mouse button for an action
 func set_action_mouse_button(target_action: String, button: MouseButton, swap_if_taken: bool = true) -> Error:
+	return _update_action_mouse_button(target_action, button, swap_if_taken)
+
+
+## Set a sepcific button for an action
+func replace_action_mouse_button(target_action: String, current_button: MouseButton, next_button: MouseButton, swap_if_taken: bool = true) -> Error:
+	return _update_action_mouse_button(target_action, next_button, swap_if_taken, current_button)
+
+
+## Set a specific button, given its index
+func replace_action_mouse_button_at_index(target_action: String, index: int, next_button: MouseButton, swap_if_taken: bool = true) -> Error:
+	var buttons: Array[MouseButton] = get_action_mouse_buttons(target_action)
+
+	if buttons.is_empty() or buttons.size() < index: return ERR_INVALID_DATA
+
+	return _update_action_mouse_button(target_action, next_button, swap_if_taken, buttons[index])
+
+
+func _update_action_mouse_button(target_action: String, button: MouseButton, swap_if_taken: bool = true, only_replace_button: MouseButton = MOUSE_BUTTON_NONE) -> Error:
 	# Find any action that is already mapped to this button
 	var clashing_action = ""
 	var clashing_event
