7cdd16a1e6b35df85046a8449a7c1b6a69468a25
Nathan Hoad <nathan@nathanhoad.net>
2023-12-08 00:14:30 +1000

    Add a test suite (#28)

---
+++ tests/test.gd
@@ -0,0 +1,17 @@
+class_name AbstractTest extends Node
+
+
+func before_all() -> void:
+	pass
+
+
+func before_each() -> void:
+	pass
+
+
+func after_all() -> void:
+	pass
+
+
+func after_each() -> void:
+	pass
