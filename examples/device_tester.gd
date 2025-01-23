470ead20a083b5038c0f4bffd493fdf1c583c2a7
Olivier PIERRE <olivier.pierre@gmail.com>
2025-01-23 07:33:12 +0100

    Update device_tester.gd
    Display InputHelper label in log
    Add steamdeck color

--- examples/device_tester.gd
+++ examples/device_tester.gd
@@ -15,27 +15,35 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if not event is InputEventMouse and event.is_pressed():
-		write_to_log("Pressed %s" % event.as_text(), InputHelper.device, InputHelper.device_index)
+		write_to_log("Pressed %s" % event.as_text(), InputHelper.device, InputHelper.device_index, InputHelper.get_label_for_input(event))
 
 
-func write_to_log(label: String, device: String, device_index: int) -> void:
+func write_to_log(label: String, device: String, device_index: int, device_input_button: String= "") -> void:
 	var color = Color.WHITE
 	match device:
 		InputHelper.DEVICE_KEYBOARD:
 			color = Color.YELLOW
-		InputHelper.DEVICE_XBOX_CONTROLLER:
+		InputHelper.DEVICE_XBOX_CONTROLLER, \
+		InputHelper.DEVICE_XBOX_ONE_CONTROLLER, \
+		InputHelper.DEVICE_XBOX_SERIES_CONTROLLER:
 			color = Color.GREEN
-		InputHelper.DEVICE_PLAYSTATION_CONTROLLER:
+		InputHelper.DEVICE_PLAYSTATION3_CONTROLLER, \
+		InputHelper.DEVICE_PLAYSTATION4_CONTROLLER, \
+		InputHelper.DEVICE_PLAYSTATION5_CONTROLLER:
 			color = Color.BLUE
 		InputHelper.DEVICE_SWITCH_CONTROLLER, \
 		InputHelper.DEVICE_SWITCH_JOYCON_LEFT_CONTROLLER, \
 		InputHelper.DEVICE_SWITCH_JOYCON_RIGHT_CONTROLLER:
 			color = Color.RED
-
+		InputHelper.DEVICE_STEAMDECK_CONTROLLER:
+			color = Color.DEEP_PINK
+	
+	var print_label = label if device_input_button == "" else "%s (%s)" % [label, device_input_button]
+	
 	if device_index > -1:
-		logger.append_text("%s: [b][color=#%s]%s[/color][/b] in slot %s\n" % [label, color.to_html(false), device, device_index])
+		logger.append_text("%s: [b][color=#%s]%s[/color][/b] in slot %s\n" % [print_label, color.to_html(false), device, device_index])
 	else:
-		logger.append_text("%s: [b][color=#%s]%s[/color][/b]\n" % [label, color.to_html(false), device])
+		logger.append_text("%s: [b][color=#%s]%s[/color][/b]\n" % [print_label, color.to_html(false), device])
 
 	logger.scroll_to_line(logger.get_line_count())
 
